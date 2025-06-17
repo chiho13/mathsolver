067a247628642852287ffd612cf2024eab29703e
Anthony Ho <ahomnky11@gmail.com>
2025-06-17 13:05:39 +0100

    Initial Commit

---
+++ videoeditorTests/videoeditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  videoeditorTests.swift
+//  videoeditorTests
+//
+//  Created by Anthony Ho on 17/06/2025.
+//
+
+import Testing
+@testable import videoeditor
+
+struct videoeditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
